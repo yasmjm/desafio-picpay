b2cafdb186cd190d2ba3ab4ecd8e91889a2aa532
yasmjm <mariayasmimsousaalmeida@gmail.com>
2026-05-23 15:47:12 -0300

    iniciando projeto

---
+++ src/main/java/br/com/picpay_desafio/PicpayDesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.picpay_desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpayDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpayDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/picpay_desafio/PicpayDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.picpay_desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
